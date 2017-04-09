9d5e4e10e92ae47d166c1b9a4a5d95e03b216365
jrharless <slkraemer@outlook.com>
2017-04-09 12:09:02 -0400

    flower with message

---
+++ Assets/Resources/Scenes/Scripts/PickableObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickableObject : MonoBehaviour {
+
+	public static Object prefab;
+	public Canvas canvas;
+	public string textToUse;
+
+	// Use this for initialization
+	void Start () {
+		prefab = Resources.Load("Prefabs/PickableObject");
+	}
+
+	void Awake() {
+		prefab = Resources.Load("Prefabs/PickableObject");
+	}
+
+	public static PickableObject Create(string newText) {
+		prefab = Resources.Load("Prefabs/PickableObject");
+		GameObject newObject = Instantiate (prefab) as GameObject;
+		PickableObject po = newObject.GetComponent<PickableObject> ();
+		po.SetText (newText);
+		return po;
+	}
+
+	public void SetText(string newText) {
+		canvas = GetComponentInChildren<Canvas> ();
+		Text text = canvas.GetComponentInChildren<Text> ();
+
+		text.text = newText;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Resources/Scenes/Scripts/PickableObjectManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObjectManager : MonoBehaviour {
+	
+	public List<string> phrases;
+
+	// Use this for initialization
+	void Start () {
+		InstantiateFlower ();
+	}
+
+	public void InstantiateFlower() {
+		PickableObject flower = PickableObject.Create (phrases [0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
