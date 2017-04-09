f12f7b5b1ebd6529fea3e7a7cef2826f6913ac9f
jrharless <slkraemer@outlook.com>
2017-04-08 22:48:03 -0400

    headset collision + VRTK

---
+++ Assets/Resources/Scripts/MovingObstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MovingObstacle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log("reached");
+
+		if (col.gameObject.GetComponent<VRTK_HeadsetCollision>()) {
+			Debug.Log("hit");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
